3c5109cd0513d6652c7face8fd6bc528d30a7236
eball0509 <161506233+eball0509@users.noreply.github.com>
2024-10-24 14:24:47 -0600

    final comments

--- SoftwareRenderer/Source/Main.cpp
+++ SoftwareRenderer/Source/Main.cpp
@@ -22,48 +22,67 @@ int main(int argc, char* argv[])
 
     Renderer* renderer = new Renderer();
 
+    // Window Creation
     renderer->Initialize();
     renderer->CreateWindow("2D", 600, 800);
 
     Framebuffer framebuffer(*renderer, renderer->GetWidth(), renderer->GetHeight());
 
+    //Loading the Background Image
     Image image;
     image.Load("background.png");
 
+
+    // Creating the camera
     Camera camera(renderer->GetWidth(), renderer->GetHeight());
     camera.SetView(glm::vec3{ 0, 0, -50 }, glm::vec3{ 0 });
     camera.SetProjection(120.0f, 800.0f / 600, 0.1f, 200.0f);
     Transform cameraTransform{ {0, 0, -20} };
 
+
+    // Creating the models: 1
     shared_ptr <Model> model = make_shared<Model>();
     model->Load("sphere.obj");
-    model->SetColor({ 165,0,127,255 });
+    model->SetColor({ 165,0,128,255 });
     Transform modelTranfsorm{ {0, 0, 0}, glm::vec3{0, 0, 90}, glm::vec3{3} };
     Actor actor(modelTranfsorm, model);
 
+    // 2
     shared_ptr <Model> model2 = make_shared<Model>();
     model2->Load("cube.obj");
     model2->SetColor({ 0,0,255,255 });
     Transform model2Tranfsorm{ {0, 0, 0}, glm::vec3{0, 0, 90}, glm::vec3{3} };
     Actor actor2(model2Tranfsorm, model2);
 
+    // 3
     shared_ptr <Model> model3 = make_shared<Model>();
     model3->Load("torus.obj");
     model3->SetColor({ 255,255,255,255 });
     Transform model3Tranfsorm{ {0, 0, 0}, glm::vec3{0, 0, 90}, glm::vec3{3} };
     Actor actor3(model3Tranfsorm, model3);
 
-
+    // Actor vector creation to load all actors
     vector<unique_ptr<Actor>> actors;
 
+
+    // pushes all actors into the vector above, allowing all to be written later
     for (int i = 0; i < 8; i++)
     {
+        // transforms for every actor: 1
         Transform transform{ {randomf(-20.0f, 20.0f), randomf(-20.0f, 20.0f), randomf(-20.0f, 20.0f)}, glm::vec3{randomf(1.0f, 180.0f)}, glm::vec3{randomf(2.0f, 7.0f)}};
+        // 2
         Transform transform2{ {randomf(-40.0f, 40.0f), randomf(-40.0f, 40.0f), randomf(-40.0f, 40.0f)}, glm::vec3{randomf(1.0f, 180.0f)}, glm::vec3{randomf(2.0f, 6.0f)} };
+        // 3
         Transform transform3{ {randomf(-40.0f, 40.0f), randomf(-40.0f, 40.0f), randomf(-40.0f, 40.0f)}, glm::vec3{randomf(1.0f, 180.0f)}, glm::vec3{randomf(2.0f, 5.0f)} };
+        
+        // Making the models actors: 1
         unique_ptr<Actor> actor = make_unique<Actor>(transform, model);
+        // 2
         unique_ptr<Actor> actor2 = make_unique<Actor>(transform2, model2);
+        // 3
         unique_ptr<Actor> actor3 = make_unique<Actor>(transform3, model3);
+        
+        // Pushing the actors onto the vector
         actors.push_back(move(actor));
         actors.push_back(move(actor2));
         actors.push_back(move(actor3));
@@ -93,35 +112,13 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor(renderer->m_renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer->m_renderer);
 
-
         framebuffer.Clear(color_t{ 0, 0, 0, 255 });
         
-
         int mx, my;
         SDL_GetMouseState(&mx, &my);
 
-        
         framebuffer.DrawImage(1,1, image);
-        //framebuffer.DrawImage(mx, my, imageAlpha);
-
-        //framebuffer.DrawLinearCurve(100, 100, 200, 200, {255,255,255,255});
-        //framebuffer.DrawQuadraticCurve(100, 200, mx, my, 300, 200, { 255,255,255,255 });
-        //framebuffer.DrawCubicCurve(100, 200, mx, my, 200, 100, 200, 200, { 255, 255, 255, 255 });
-        //PostProcess::Invert(framebuffer.m_buffer);
-        //PostProcess::Monochrome(framebuffer.m_buffer);
-        //PostProcess::Brightness(framebuffer.m_buffer, -10);
-        //PostProcess::ColorBalance(framebuffer.m_buffer, 200, 50, -50);
-        //PostProcess::NoiseEffect(framebuffer.m_buffer, 20);
-        //PostProcess::Threshold(framebuffer.m_buffer, 150);
-        //PostProcess::Posterization(framebuffer.m_buffer, 6);
-
-        //PostProcess::BoxBlur(framebuffer.m_buffer, framebuffer.m_width, framebuffer.m_height);
-        //PostProcess::GaussianBlur(framebuffer.m_buffer, framebuffer.m_width, framebuffer.m_height);
-        //PostProcess::Sharpen(framebuffer.m_buffer, framebuffer.m_width, framebuffer.m_height);
-        //PostProcess::Edge(framebuffer.m_buffer, framebuffer.m_width, framebuffer.m_height, 6);
-        //PostProcess::Emboss(framebuffer.m_buffer, framebuffer.m_width, framebuffer.m_height);
-
-
+        
         if (input.GetMouseButtonDown(2)) {
             input.SetRelativeMode(true);
 
@@ -145,6 +142,7 @@ int main(int argc, char* argv[])
       
         camera.SetView(cameraTransform.position, cameraTransform.position + cameraTransform.GetForward());
 
+        // Draws every actor 
         for (auto& actor : actors) {
             actor->Draw(framebuffer, camera);
         }
